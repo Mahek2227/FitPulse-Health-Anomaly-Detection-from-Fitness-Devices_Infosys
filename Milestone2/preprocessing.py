430095236883990c76c9cc2109f48b301ff67121
Mahek2227 <maheksultana2004@gmail.com>
2025-12-27 18:46:44 +0530

    Delete Milestone2/preprocessing.py

--- Milestone2/preprocessing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def preprocess_csv(input_path, output_path):
-    df = pd.read_csv(input_path)
-    df.columns = df.columns.str.lower().str.replace(" ", "_")
-    df['timestamp'] = pd.to_datetime(df['timestamp'], errors='coerce')
-    df = df.dropna()
-    df.to_csv(output_path, index=False)
-    return df
