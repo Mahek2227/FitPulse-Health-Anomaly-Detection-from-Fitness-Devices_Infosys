298fa10b050a7bb54df0d4e4a9c950de5f27e62b
Mahek2227 <maheksultana2004@gmail.com>
2025-12-27 18:46:32 +0530

    Delete Milestone2/modeling.py

--- Milestone2/modeling.py
+++
@@ -1,9 +0,0 @@
-from prophet import Prophet
-from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
-
-def prophet_model(df):
-    model = Prophet(daily_seasonality=True)
-    model.fit(df)
-    future = model.make_future_dataframe(periods=30)
-    return model.predict(future)
